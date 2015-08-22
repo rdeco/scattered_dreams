ae60ccf859f84b9f82e2bbf72323b34edb996f4c
rdeco <rdecodesigns@gmail.com>
2015-08-22 12:56:08 -1000

    added if statemet to disable jquery on mobile devices

--- js/ninja-404.js
+++ js/ninja-404.js
@@ -1,14 +1,14 @@
-
-$('#ninja').hide().slideToggle(2000, function(){
-	$('#ninja').hide(1500);
-});
-
-$('#msg').hide().show(4000, function(){
-	$('#msg').hide("slow");
-});
-
-$('#ninja-head').hide().slideToggle(8500);
-
-
-$( "#search-form-404").hide().slideToggle(10000);
-
+if ( $(window).width() > 767) {
+	$('#ninja').hide().slideToggle(2000, function(){
+		$('#ninja').hide(1500);
+	});
+	
+	$('#msg').hide().show(4000, function(){
+		$('#msg').hide("slow");
+	});
+	
+	$('#ninja-head').hide().slideToggle(8500);
+	
+	
+	$( "#search-form-404").hide().slideToggle(9500);
+}
