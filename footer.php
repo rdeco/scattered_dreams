08e0ae47e36eab106c703863e416bb62ed24b609
rdeco <rdecodesigns@gmail.com>
2015-08-25 16:23:06 -1000

    cleaned up white space & took out container-fluid class

--- footer.php
+++ footer.php
@@ -1,41 +1,35 @@
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-xs-12 footer">
-						<div class="footer-gradient">
-
-								<div class="hidden-xs col-md-4 col-sm-12 footer-col-1">
-									<blockquote>								
-										<?php if (dynamic_sidebar('footer-left')); ?>							
-									</blockquote>
-								</div>
-								
-								<div class="col-xs-12 col-md-4 col-sm-6 footer-col-2">
-									<a class="contact-me" href="http://www.rdecodesigns.com/scattered-dreams/contact-page/">									
-										<span>Contact Me <i class="fa fa-pencil-square-o"></i></span>
-									</a>
-								</div>
-						
-								<div class="col-xs-12 col-md-4 col-sm-6 footer-col-3">									
-									<div class="footer-about-info">
-										<a href="<?php bloginfo('url'); ?>/about">
-											<h1>About Me</h1>
-											<p class="footer-about-excerpt">Catnipsum dolor sit amet fluffy daisy tabby catnipsum catnip meow. Catnipsum twitchy ears kitty-litter lucky mrow let the cat out of the bag. Mrowwwww mrooww lazy
-											</p>
-											
-											<img src="<?php print IMAGES; ?>/cat-king.png" alt="img02">										
-										</a>
-									</div>															
-								</div>
-
-								<div class="col-xs-12 footer-copyright">																				<p class="footer-content">
-										© r.deco designs • All Rights Reserved 2015
+			<div class="row">
+				<div class="col-xs-12 footer">
+					<div class="footer-gradient">
+						<div class="hidden-xs col-md-4 col-sm-12 footer-col-1">
+							<blockquote>								
+								<?php if (dynamic_sidebar('footer-left')); ?>							
+							</blockquote>
+						</div>						
+						<div class="col-xs-12 col-md-4 col-sm-6 footer-col-2">
+							<a class="contact-me" href="http://www.rdecodesigns.com/scattered-dreams/contact-page/">									
+								<span>Contact Me <i class="fa fa-pencil-square-o"></i></span>
+							</a>
+						</div>				
+						<div class="col-xs-12 col-md-4 col-sm-6 footer-col-3">									
+							<div class="footer-about-info">
+								<a href="<?php bloginfo('url'); ?>/about">
+									<h1>About Me</h1>
+									<p class="footer-about-excerpt">Catnipsum dolor sit amet fluffy daisy tabby catnipsum catnip meow. Catnipsum twitchy ears kitty-litter lucky mrow let the cat out of the bag. Mrowwwww mrooww lazy
 									</p>
-								</div>
-							</div>									
-						</div>											
-					</div>
-				</div>			
-			</div>
+									
+									<img src="<?php print IMAGES; ?>/cat-king.png" alt="img02">										
+								</a>
+							</div>															
+						</div>
+						<div class="col-xs-12 footer-copyright">																				
+							<p class="footer-content">
+								© r.deco designs • All Rights Reserved 2015
+							</p>
+						</div>																		
+					</div>	<!-- .footer-gradient -->										
+				</div>	<!-- col-xs-12 .footer -->				
+			</div> <!-- .row -->
 		<?php wp_footer(); ?>
 	</body>
 </html>
