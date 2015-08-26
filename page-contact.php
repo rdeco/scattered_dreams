4c588f6c94196e46a0c25890b9b347aa33219739
rdeco <rdecodesigns@gmail.com>
2015-08-26 09:54:27 -1000

    cleaned up white space

--- page-contact.php
+++ page-contact.php
@@ -5,31 +5,25 @@
 
 	get_header(); ?>
 
-
-
-<div class="container-fluid">
-
-	<h1 class="contact-h1"><?php the_title(); ?></h1>	
-	
+	<div class="container-fluid">
+		<h1 class="contact-h1"><?php the_title(); ?></h1>	
 		<div class="row">
-				<div id="contact-content" class="col-xs-12 contact-content">				
-			<?php
-				if(have_posts()){
-					while(have_posts()){
-						the_post();	
-						the_content();
-
-							}	
-						}
-					?>
-				</div>
-				<?php get_footer(); ?>
+			<div id="contact-content" class="col-xs-12 contact-content">				
+				<?php
+					if(have_posts()){
+						while(have_posts()){
+							the_post();	
+							the_content();
+		
+								}	
+							}
+				?>
 			</div>
-		</div><!-- .col-xs-12 .wrapper -->
+			<?php get_footer(); ?>
+		</div>
 	</div>
-</div>
-
-				
+	
+		
 
 		
 		
